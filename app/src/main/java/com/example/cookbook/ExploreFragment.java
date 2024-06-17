5b4f2d0279e83f3924c58c469d7b5c353f7ba509
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-17 12:21:03 -0400

    initialize fragments

---
+++ app/src/main/java/com/example/cookbook/ExploreFragment.java
@@ -0,0 +1,31 @@
+package com.example.cookbook;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ExploreFragment extends Fragment {
+
+    public ExploreFragment() {}
+
+    public static ExploreFragment newInstance() {
+        return new ExploreFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/cookbook/NewFragment.java
@@ -0,0 +1,31 @@
+package com.example.cookbook;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewFragment extends Fragment {
+
+    public NewFragment() {}
+
+    public static NewFragment newInstance() {
+        return new NewFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_new, container, false);
+
+
+        return view;
+    }
+}
