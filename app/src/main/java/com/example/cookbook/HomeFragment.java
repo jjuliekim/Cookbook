a57684dce0dae589c3257a645e052ab8827c8e52
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-17 13:49:15 -0400

    home fragment

--- app/src/main/java/com/example/cookbook/HomeFragment.java
+++ app/src/main/java/com/example/cookbook/HomeFragment.java
@@ -2,14 +2,24 @@ package com.example.cookbook;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -28,13 +38,50 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recipeDatabase = FirebaseDatabase.getInstance().getReference("recipes");
+        // set recycler view
+        recipeAdapter = new RecipeAdapter(getContext(), new ArrayList<>());
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(recipeAdapter);
 
+        fetchRecipesFromUser();
 
         return view;
     }
+
+    // fetch from db recipes created by user
+    private void fetchRecipesFromUser() {
+        recipeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                recipeList = new ArrayList<>();
+                try {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Recipe recipe = snapshot.getValue(Recipe.class);
+                        if (recipe.getUser().equals(user.getUid())) {
+                            recipeList.add(recipe);
+                        }
+                    }
+                    recipeAdapter.updateRecipes(recipeList);
+                    Log.i("HERE HOME", "loaded " + recipeList.size());
+                } catch (Exception e) {
+                    Log.i("HERE HOME", "fetching e: " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.i("HERE HOME", "failed to load places");
+                Toast.makeText(getContext(), "failed to load places", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
