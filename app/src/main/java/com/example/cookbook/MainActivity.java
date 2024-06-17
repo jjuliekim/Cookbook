a277ebf66bf9f340502085af166268764cb4632e
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-17 12:17:33 -0400

    main page to hold fragments

--- app/src/main/java/com/example/cookbook/MainActivity.java
+++ app/src/main/java/com/example/cookbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,8 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent myIntent = getIntent();
+        FirebaseUser user = myIntent.getParcelableExtra("user");
+
+        // initial fragment
+        bundle = new Bundle();
+        bundle.putParcelable("user", user);
+        replaceFragment(HomeFragment.newInstance());
+
+        // action on selected tab
+        TabLayout tabLayout = findViewById(R.id.topMenuTabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment selectedFragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case 1:
+                        selectedFragment = new ExploreFragment();
+                        break;
+                    case 2:
+                        selectedFragment = new NewFragment();
+                        break;
+                    case 3:
+                        selectedFragment = new SettingsFragment();
+                        break;
+                }
+                if (selectedFragment != null) {
+                    replaceFragment(selectedFragment);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+
+    // switch fragments
+    private void replaceFragment(Fragment fragment) {
+        fragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
