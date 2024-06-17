a9dcb93f84ffb0578b0e86908a961f1a55dccb80
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-17 14:32:10 -0400

    new fragment

--- app/src/main/java/com/example/cookbook/NewFragment.java
+++ app/src/main/java/com/example/cookbook/NewFragment.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewFragment extends Fragment {
+    private EditText recipeInputText;
+    private TextView searchRecipeText;
 
     public NewFragment() {}
 
@@ -24,8 +30,28 @@ public class NewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new, container, false);
-
-
+        recipeInputText = view.findViewById(R.id.recipeInputNameText);
+        searchRecipeText = view.findViewById(R.id.searchRecipeText);
+        searchRecipeText.setOnClickListener(v -> searchRecipe());
+        Button saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(v -> saveRecipe());
         return view;
     }
+
+    // search recipe info on API
+    private void searchRecipe() {
+        String recipeName = recipeInputText.getText().toString();
+        if (recipeName.isEmpty()) {
+            Toast.makeText(getContext(), "Input Recipe Name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // search recipe API
+
+    }
+
+    // save recipe to database
+    private void saveRecipe() {
+
+    }
+
 }
