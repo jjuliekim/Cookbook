786059a513584d340574d024ae70e7756304b457
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-06-19 14:53:50 -0400

    settings xml

--- app/src/main/java/com/example/cookbook/SettingsFragment.java
+++ app/src/main/java/com/example/cookbook/SettingsFragment.java
@@ -25,7 +25,6 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-
         return view;
     }
 }
